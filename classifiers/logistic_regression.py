55a16d5fb7b75df2aaa142bf06f44735838c86ed
Zhinan Guan <zhinanguan@wifi-131-179-43-98.host.ucla.edu>
2015-11-30 21:16:57 -0800

    add lr_submission.csv

--- classifiers/logistic_regression.py
+++ classifiers/logistic_regression.py
@@ -7,10 +7,17 @@ print "Loading mat data"
 data = sio.loadmat('../data/train_encoded.mat')
 X = data['features']
 Y = data['labels'][0]
-
+testData = sio.loadmat('../data/test_encoded.mat')
+testX = testData['features']
+ids = testData['ids']
+ 
 # Fit model
 print "Training..."
-for reg in [10, 25, 50, 100, 150, 200]:
-	classifier = linear_model.LogisticRegression(C = reg)
-	classifier.fit(X[1:30000], Y[1:30000])
-	print reg, classifier.score(X[30000:39774], Y[30000:39774])
+classifier = linear_model.LogisticRegression()
+classifier.fit(X, Y)
+prediction = classifier.predict(testX)
+
+# Save data
+testId = np.array(ids)
+predicts = np.array(prediction)
+sio.savemat('../lr_prediction.mat', {'ids': ids, 'predicts': predicts})
