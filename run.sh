562aeffbbd496084678a3695d31dfc3c18672120
Zhixin Wen <john.wenzhixin@gmail.com>
2015-11-30 23:39:49 -0800

    update run.sh for logistic_regression.py

--- run.sh
+++ run.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 python encodeData.py ./data/train.json
+mv encoding_output.mat ./data/train_encoded.mat
+python encodeData.py ./data/test.json
+mv encoding_output.mat ./data/test_encoded.mat
+
 cd ./classifiers
 
 echo "Result from svm algorithm is:"
 python svm.py
-echo "Result from logistic regression algorithm is:"
+
+echo "Result from logistic regression is:"
 python logistic_regression.py
 
 cd ..
